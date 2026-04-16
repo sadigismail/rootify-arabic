1dd33f326c02934d7bed09524320b48685da90d8
sadigismail <sadigismail@yahoo.com>
2026-04-15 19:24:48 -0700

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -39,7 +39,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (_req, res) => res.redirect("/api/smartroot/"
 app.use("/api", router);
 
 export default app;
